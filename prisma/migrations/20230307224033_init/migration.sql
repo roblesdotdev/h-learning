24373c9a16731a24f62c2a6225beddbdb39f2a3c
Aldo R. Robles <90198035+roblesdotdev@users.noreply.github.com>
2023-03-07 19:42:38 -0300

    update datamodel (#2)

---
+++ prisma/migrations/20230307224033_init/migration.sql
@@ -0,0 +1,45 @@
+-- CreateTable
+CREATE TABLE "Country" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "countryId" TEXT,
+    CONSTRAINT "User_countryId_fkey" FOREIGN KEY ("countryId") REFERENCES "Country" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "ContactInfo" (
+    "userId" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "github" TEXT NOT NULL,
+    CONSTRAINT "ContactInfo_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Password" (
+    "userId" TEXT NOT NULL,
+    "hash" TEXT NOT NULL,
+    CONSTRAINT "Password_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ContactInfo_userId_key" ON "ContactInfo"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Password_userId_key" ON "Password"("userId");
--- prisma/seed/utils.ts
+++ prisma/seed/utils.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker'
 import bcrypt from 'bcryptjs'
 import type { User, Password } from '~/types'
 
@@ -12,4 +13,28 @@ export function createPassword(username: string): Pick<Password, 'hash'> {
   return {
     hash: bcrypt.hashSync(username.toUpperCase(), 10),
   }
+}
+
+export function createContactInfo(username: string) {
+  return {
+    firstName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    phone: faker.phone.number(),
+    github: `https://github.com/${username}`,
+  }
+}
+
+export function getCountryList() {
+  return [
+    'Argentina',
+    'Bolivia',
+    'Brasil',
+    'Chile',
+    'Colombia',
+    'Ecuador',
+    'Paraguary',
+    'Peru',
+    'Uruguay',
+    'Venezuela',
+  ]
 }
